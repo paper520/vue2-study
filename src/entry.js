03d8e1f9bbffd15419e6e675817b64eba92ab340
心叶 <yelloxing@gmail.com>
2019-02-22 10:09:22 +0800

    备份提交

--- src/entry.js
+++ src/entry.js
@@ -5,7 +5,14 @@ import VeeValidate from 'vee-validate';
 // 引入基础css
 import '../node_modules/easycss-core/build/easycss.min.css';
 
-Vue.use(VeeValidate);
+const config = {
+    // errorBagName: 'errors',// 代表自定义errors
+    delay: 0,// 代表输入多少ms之后进行校验
+    locale: 'zh_CN',
+    messages: null,// 代表自定义校验信息
+    strict: true // 代表没有设置规则的表单不进行校验
+};
+Vue.use(VeeValidate, config);
 
 //根对象
 var vm = new Vue({
--- src/pages/vee-validate.vue
+++ src/pages/vee-validate.vue
@@ -1,13 +1,32 @@
 <template>
-  
+  <form>
+    <div>
+      <label for="email">邮箱</label>
+      <input v-validate v-model="email" data-rules="required|email" name="email" type="text" />
+    </div>
+    <hr /> {{errors.first('email')}}
+    <hr />
+    <input type="button" value="点击查看" @click="doIt()">
+  </form>
 </template>
 
 <script>
 export default {
-  
-}
+  data() {
+    return {
+      email: ""
+    };
+  },
+  methods: {
+    doIt() {
+      console.log(this.errors);
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
+form {
+  margin: 1rem;
+}
 </style>
